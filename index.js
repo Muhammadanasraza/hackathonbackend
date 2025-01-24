68445053112f5d6fa8baf6293b40657ea8237de0
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2025-01-25 03:53:46 +0500

    push

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+// import dotenv from "dotenv";
+// import connectDB from "./database/db.js";
+// import user from "./routers/user.route.js";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+
+// Load environment variables
+// dotenv.config({});
+
+// Connect to the database
+// connectDB();
+
+const app = express();
+const PORT = process.env.PORT || 9000;
+
+// Default middlewares
+app.use(express.json());
+// app.use(cookieParser());
+// app.use(cors());
+
+// API Routes
+// app.use("/api/v1/user", user);
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
